7c309fa8b14436c7693d7d5eafdb3e0eff046bd9
Cristina <gre.cristina20@gmail.com>
2023-07-21 15:45:57 -0600

    ref to products

--- src/js/addProduct.js
+++ src/js/addProduct.js
@@ -3,7 +3,7 @@ import { validateForm } from './validate-form.js'
 
 validateForm()
 
-const formAdd = document.querySelector('[data-formadd]')
+const formAdd = document.getElementById('form-add')
 
 formAdd.addEventListener('submit', (event) => {
   event.preventDefault()
@@ -17,14 +17,21 @@ formAdd.addEventListener('submit', (event) => {
   clientServices
     .createProduct(nameProduct, imageURL, description, category, price)
     .then((response) => {
-      const modal = document.querySelector('[data-modal]')
-      modal.style.display = 'flex'
-      window.location.href = '../products.html'
-      modal.classList.add('modal-active')
-      window.addEventListener('click', (event) => {
-        if (event.target == modal) {
-          modal.classList.remove('modal-active')
-        }
-      })
+      // const modal = document.getElementById('modal-exit')
+      // modal.style.display = 'flex'
+      window.location.href = './products.html'
+      // modal.classList.add('modal-active')
+      // setTimeout(() => {
+      //   window.addEventListener(
+      //     'click',
+      //     (event) => {
+      //       if (event.target == modal) {
+      //         // modal.classList.remove('modal-active')
+      //         modal.style.display = 'flex'
+      //       }
+      //     },
+      //     1000
+      //   )
+      // })
     })
 })
--- src/js/register.js
+++ src/js/register.js
@@ -19,12 +19,6 @@ form.addEventListener('submit', (event) => {
   const nameUser = document.querySelector('[data-user]').value
   const password = document.querySelector('[data-password]').value
 
-  // if ((nameUser === 'admin') & (password === '123456')) {
-  //   console.log(nameUser, password)
-  //   window.location.href = './products.html'
-  // } else {
-  //   alert('Hay un error en el nombre o contraseña')
-  // }
   for (var i = 0; i < peopleAccess.length; i++) {
     if (
       nameUser == peopleAccess[i].username &&
